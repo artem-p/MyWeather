c0abf1714a513cb5ed4c38dd721ad1f6f756f3e2
artem-p <artempugachev1@gmail.com>
2017-05-24 17:40:31 +0300

    binding

--- app/src/main/java/ru/artempugachev/myweather/MainActivity.kt
+++ app/src/main/java/ru/artempugachev/myweather/MainActivity.kt
@@ -1,12 +1,17 @@
 package ru.artempugachev.myweather
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ru.artempugachev.myweather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        // todo finish binding with example object
     }
 }
 
