711e1ca7f0644d501c1bf8dd7ace73d34faf8ecd
artem-p <artempugachev1@gmail.com>
2017-05-29 17:34:07 +0300

    custom attribute for wind arrow

--- app/src/main/java/ru/artempugachev/myweather/WindArrow.kt
+++ app/src/main/java/ru/artempugachev/myweather/WindArrow.kt
@@ -1,6 +1,7 @@
 package ru.artempugachev.myweather
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
@@ -10,11 +11,24 @@ import android.widget.ImageView
  * Component for wind arrow
  */
 
-class WindArrow(context: Context?) : FrameLayout(context) {
-    constructor(context: Context?, attrs: AttributeSet?) : this(context) {
+class WindArrow(context: Context) : FrameLayout(context) {
+    constructor(context: Context, attrs: AttributeSet?) : this(context) {
         val inflater: LayoutInflater = LayoutInflater.from(context)
         inflater.inflate(R.layout.wind_arrow, this)
 
+        val attrArray: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.WindArrow)
+        val windDir: Int = attrArray.getInt(R.styleable.WindArrow_wind_dir, 0)
+        attrArray.recycle()
+
+        setDirection(windDir)
+    }
+
+    /**
+     * Set wind arrow by wind direction
+     * */
+    fun setDirection(direction: Int) {
+        // stub
         val arrowImageView: ImageView = findViewById(R.id.windArrowImageView) as ImageView
+        arrowImageView.setImageResource(R.drawable.wind_south_west)
     }
 }
