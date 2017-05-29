eae364ee677037a55133669ee007323abec898f3
artem-p <artempugachev1@gmail.com>
2017-05-29 18:15:44 +0300

    wind test stub

---
+++ app/src/test/java/ru/artempugachev/myweather/WindTest.kt
@@ -0,0 +1,12 @@
+package ru.artempugachev.myweather
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class WindTest {
+    @Test
+    fun test_wind_arrow_resource() {
+        // todo stub
+        assertEquals(4, 2 + 2)
+    }
+}
