8b2dad1bfe7871b1e578c3f1d7090d1a8c8ff607
artem-p <artempugachev1@gmail.com>
2017-05-31 18:25:22 +0300

    db helper test stub

---
+++ app/src/androidTest/java/ru/artempugachev/myweather/DbHelperTest.kt
@@ -0,0 +1,31 @@
+package ru.artempugachev.myweather
+
+import android.content.Context
+import android.database.sqlite.SQLiteOpenHelper
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class DbHelperTest {
+    val context: Context = InstrumentationRegistry.getTargetContext()
+
+    @Before
+    fun setUp() {
+        deleteDb()
+    }
+
+    @Test
+    fun testCreateDb() {
+        val dbHelper = WeatherDbHelper(context)
+        assertEquals(4, 2 + 2)
+    }
+
+    fun deleteDb() {
+        context.deleteDatabase(DATABASE_NAME)
+    }
+
+}
