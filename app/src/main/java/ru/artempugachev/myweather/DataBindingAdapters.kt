4fa64c6948dcd5f3fda4ee93bb0ff1c0d9b0a7d7
artem-p <artempugachev1@gmail.com>
2017-05-31 12:17:54 +0300

    add parameters to weather contract

--- app/src/main/java/ru/artempugachev/myweather/DataBindingAdapters.kt
+++ app/src/main/java/ru/artempugachev/myweather/DataBindingAdapters.kt
@@ -1,3 +1,14 @@
 package ru.artempugachev.myweather
 
-class DataBindingsAdapters
+import android.databinding.BindingAdapter
+import android.widget.ImageView
+
+class DataBindingAdapters {
+    companion object {
+        @BindingAdapter("android:src")
+        @JvmStatic
+        fun setImageResource(imageView: ImageView, imageResource: Int) {
+            imageView.setImageResource(imageResource)
+        }
+    }
+}
--- app/src/main/java/ru/artempugachev/myweather/WeatherContract.kt
+++ app/src/main/java/ru/artempugachev/myweather/WeatherContract.kt
@@ -6,7 +6,12 @@ class WeatherContract private constructor(){
     class WeatherEntry : BaseColumns {
         val TABLE_NAME: String = "weather"
         val COLUMN_TIMESTAMP = "timestamp"
+        val COLUMN_FEELS_LIKE_TEMPERATURE = "feels_like_temperature"
         val COLUMN_TEMPERATURE = "temperature"
+        val COLUMN_HUMIDITY = "humidity"
+        val COLUMN_PRESSURE = "pressure"
         val COLUMN_WEATHER_CODE = "weather_code"
+        val COLUMN_WIND_DIRECTION = "wind_direction"
+        val COLUMN_WIND_SPEED = "wind_speed"
     }
 }
