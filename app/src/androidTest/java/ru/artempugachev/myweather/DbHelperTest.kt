3458dfae9c3ed4c73cce3a0302af3593ee222d03
artem-p <artempugachev1@gmail.com>
2017-06-01 12:07:15 +0300

    write read db test

--- app/src/androidTest/java/ru/artempugachev/myweather/DbHelperTest.kt
+++ app/src/androidTest/java/ru/artempugachev/myweather/DbHelperTest.kt
@@ -1,5 +1,6 @@
 package ru.artempugachev.myweather
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -14,17 +15,20 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class DbHelperTest {
     val context: Context = InstrumentationRegistry.getTargetContext()
-    val dbHelper: WeatherDbHelper
-    val db: SQLiteDatabase
+    var dbHelper: WeatherDbHelper
+    var db: SQLiteDatabase
 
     init {
         dbHelper = WeatherDbHelper(context)
         db = dbHelper.writableDatabase
     }
 
+
     @Before
     fun setUp() {
         deleteDb()
+        dbHelper = WeatherDbHelper(context)
+        db = dbHelper.writableDatabase
     }
 
     /**
@@ -41,7 +45,34 @@ class DbHelperTest {
      * */
     @Test
     fun testDbWriteRead() {
+        val cv: ContentValues = ContentValues()
+        cv.put(WeatherContract.WeatherEntry._ID, 1)
+        cv.put(WeatherContract.WeatherEntry.COLUMN_TIMESTAMP, 111)
+        cv.put(WeatherContract.WeatherEntry.COLUMN_FEELS_LIKE_TEMPERATURE, 14.3)
+        cv.put(WeatherContract.WeatherEntry.COLUMN_TEMPERATURE, 11.3)
+        cv.put(WeatherContract.WeatherEntry.COLUMN_HUMIDITY, 75)
+        cv.put(WeatherContract.WeatherEntry.COLUMN_PRESSURE, 1015.5)
+        cv.put(WeatherContract.WeatherEntry.COLUMN_WEATHER_CODE, 35)
+        cv.put(WeatherContract.WeatherEntry.COLUMN_WIND_DIRECTION, 270)
+        cv.put(WeatherContract.WeatherEntry.COLUMN_WIND_SPEED, 5)
 
+        val insertedNumber = db.insert(WeatherContract.WeatherEntry.TABLE_NAME, null, cv)
+        assertEquals(1, insertedNumber)
+
+        val projection: Array<String> = arrayOf(WeatherContract.WeatherEntry._ID,
+                WeatherContract.WeatherEntry.COLUMN_TIMESTAMP)
+
+        val cursor = db.query(WeatherContract.WeatherEntry.TABLE_NAME,
+                projection, null, null, null, null, null)
+        cursor.moveToFirst()
+
+        val id = cursor.getInt(cursor.getColumnIndex(WeatherContract.WeatherEntry._ID))
+        val timestamp = cursor.getInt(cursor.getColumnIndex(WeatherContract.WeatherEntry.COLUMN_TIMESTAMP))
+
+        assertEquals(1, id)
+        assertEquals(111, timestamp)
+
+        cursor.close()
     }
 
     fun deleteDb() {
