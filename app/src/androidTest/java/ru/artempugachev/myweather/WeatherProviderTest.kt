785b469f9b7b6eaee9fca9d2e4d238e350edeac2
artem-p <artempugachev1@gmail.com>
2017-06-02 15:49:29 +0300

    test add read rows with content provider

--- app/src/androidTest/java/ru/artempugachev/myweather/WeatherProviderTest.kt
+++ app/src/androidTest/java/ru/artempugachev/myweather/WeatherProviderTest.kt
@@ -1,8 +1,7 @@
 package ru.artempugachev.myweather
 
-import android.content.ContentResolver
 import android.content.ContentValues
-import android.content.Context
+import android.database.Cursor
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 import android.test.ProviderTestCase2
@@ -28,24 +27,58 @@ class WeatherProviderTest : ProviderTestCase2<WeatherContentProvider>(
      * Add test rows and then read
      * */
     @Test
-    fun testAddReadRows() {
+    fun testReadRows() {
+        testAddRows()
+        val cursor: Cursor = context.contentResolver.query(WEATHER_URI,
+                null, null, null, null)
+
+        assertEquals(3, cursor.count)
+
+        cursor.close()
+    }
+
+    @Test
+    fun testAddRows() {
+        val rowInserted = addRows()
+        assertEquals(3, rowInserted)
+    }
+
+    fun addRows(): Int {
         val cv1: ContentValues = ContentValues()
         cv1.put(WeatherContract.WeatherEntry._ID, 1)
         cv1.put(WeatherContract.WeatherEntry.COLUMN_TIMESTAMP, 111)
-        cv1.put(WeatherContract.WeatherEntry.COLUMN_TEMPERATURE, 11)
+        cv1.put(WeatherContract.WeatherEntry.COLUMN_TEMPERATURE, 11.3)
+        cv1.put(WeatherContract.WeatherEntry.COLUMN_FEELS_LIKE_TEMPERATURE, 14.3)
+        cv1.put(WeatherContract.WeatherEntry.COLUMN_HUMIDITY, 75)
+        cv1.put(WeatherContract.WeatherEntry.COLUMN_PRESSURE, 1015.5)
+        cv1.put(WeatherContract.WeatherEntry.COLUMN_WEATHER_CODE, 35)
+        cv1.put(WeatherContract.WeatherEntry.COLUMN_WIND_DIRECTION, 270)
+        cv1.put(WeatherContract.WeatherEntry.COLUMN_WIND_SPEED, 5)
 
         val cv2: ContentValues = ContentValues()
         cv2.put(WeatherContract.WeatherEntry._ID, 2)
         cv2.put(WeatherContract.WeatherEntry.COLUMN_TIMESTAMP, 222)
         cv2.put(WeatherContract.WeatherEntry.COLUMN_TEMPERATURE, -11.2)
+        cv2.put(WeatherContract.WeatherEntry.COLUMN_FEELS_LIKE_TEMPERATURE, 14.3)
+        cv2.put(WeatherContract.WeatherEntry.COLUMN_HUMIDITY, 75)
+        cv2.put(WeatherContract.WeatherEntry.COLUMN_PRESSURE, 1015.5)
+        cv2.put(WeatherContract.WeatherEntry.COLUMN_WEATHER_CODE, 35)
+        cv2.put(WeatherContract.WeatherEntry.COLUMN_WIND_DIRECTION, 270)
+        cv2.put(WeatherContract.WeatherEntry.COLUMN_WIND_SPEED, 5)
 
         val cv3: ContentValues = ContentValues()
         cv3.put(WeatherContract.WeatherEntry._ID, 3)
         cv3.put(WeatherContract.WeatherEntry.COLUMN_TIMESTAMP, 333)
         cv3.put(WeatherContract.WeatherEntry.COLUMN_TEMPERATURE, 11.3)
+        cv3.put(WeatherContract.WeatherEntry.COLUMN_FEELS_LIKE_TEMPERATURE, 14.3)
+        cv3.put(WeatherContract.WeatherEntry.COLUMN_HUMIDITY, 75)
+        cv3.put(WeatherContract.WeatherEntry.COLUMN_PRESSURE, 1015.5)
+        cv3.put(WeatherContract.WeatherEntry.COLUMN_WEATHER_CODE, 35)
+        cv3.put(WeatherContract.WeatherEntry.COLUMN_WIND_DIRECTION, 270)
+        cv3.put(WeatherContract.WeatherEntry.COLUMN_WIND_SPEED, 5)
 
         val valArray: Array<ContentValues> = arrayOf(cv1, cv2, cv3)
-        context.contentResolver.bulkInsert(WEATHER_URI, valArray)
+        return context.contentResolver.bulkInsert(WEATHER_URI, valArray)
     }
 
     fun deleteAllRows() {
