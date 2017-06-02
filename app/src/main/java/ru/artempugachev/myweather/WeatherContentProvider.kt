6f1d2408e228d6b55085a3c549cccfee05b46010
artem-p <artempugachev1@gmail.com>
2017-06-02 12:18:32 +0300

    fix onCreate in contentprovider

--- app/src/main/java/ru/artempugachev/myweather/WeatherContentProvider.kt
+++ app/src/main/java/ru/artempugachev/myweather/WeatherContentProvider.kt
@@ -23,7 +23,7 @@ class WeatherContentProvider : ContentProvider() {
 
     override fun onCreate(): Boolean {
         dbHelper = WeatherDbHelper(context)
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return true
     }
 
 
