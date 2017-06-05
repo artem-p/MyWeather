08f9d89dbade7350bb52fa0b2979795d487c7267
artem-p <artempugachev1@gmail.com>
2017-06-05 12:58:59 +0300

    update binding in deliver result

--- app/src/main/java/ru/artempugachev/myweather/MainActivity.kt
+++ app/src/main/java/ru/artempugachev/myweather/MainActivity.kt
@@ -1,11 +1,9 @@
 package ru.artempugachev.myweather
 
-import android.database.Cursor
 import android.databinding.DataBindingUtil
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.LoaderManager
 import android.support.v4.app.LoaderManager.LoaderCallbacks
 import android.support.v4.content.AsyncTaskLoader
 import android.support.v4.content.Loader
@@ -15,6 +13,7 @@ val WEATHER_LOADER_ID = 42
 
 class MainActivity : AppCompatActivity(), LoaderCallbacks<WeatherData> {
     lateinit var binding: ActivityMainBinding
+    lateinit var curWeatherData: WeatherData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +21,9 @@ class MainActivity : AppCompatActivity(), LoaderCallbacks<WeatherData> {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val weather: Weather = Weather(1, 14.4, 5, Wind(0.0, 0))
-        val weatherData: WeatherData = weather.toWeatherData()
+        curWeatherData = weather.toWeatherData()
 
-        binding.weatherData = weatherData
+        binding.weatherData = curWeatherData
 
         loadTestData()
     }
@@ -74,8 +73,9 @@ class MainActivity : AppCompatActivity(), LoaderCallbacks<WeatherData> {
             return dataProvider.getCurrentData()
         }
 
-        override fun deliverResult(data: WeatherData?) {
-            super.deliverResult(data)
+        override fun deliverResult(weatherData: WeatherData?) {
+            if (weatherData != null) curWeatherData = weatherData
+            super.deliverResult(weatherData)
         }
     }
 }
