508f13c7a7a0846a98eb5a34f3fb9fdd95b995d9
artem-p <artempugachev1@gmail.com>
2017-06-06 12:06:09 +0300

    copy coordinate class

---
+++ app/src/main/java/ru/artempugachev/myweather/Coordinate.kt
@@ -0,0 +1,17 @@
+package ru.artempugachev.myweather
+
+/**
+ * Coordinate with String parameters
+ * We need to read coordinates from preferences and put it to url to fetch weather
+ * So we need Strings
+ */
+
+class Coordinate(private val lat: String, private val lon: String) {
+
+    /**
+     * Return String with weather URl path (60, 30)
+     */
+    fun toUrlPath(): String {
+        return lat + "," + lon
+    }
+}
