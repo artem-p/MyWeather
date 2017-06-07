c2d2ec0882495e99c36835f07e80ce07caa1fee4
artem-p <artempugachev1@gmail.com>
2017-06-07 15:39:45 +0300

    blank forecast activity

---
+++ app/src/main/java/ru/artempugachev/myweather/ForecastActivity.kt
@@ -0,0 +1,11 @@
+package ru.artempugachev.myweather
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ForecastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forecast)
+    }
+}
