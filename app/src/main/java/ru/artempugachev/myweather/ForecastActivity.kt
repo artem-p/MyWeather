596ad79552bdfc47fa1f0fef382b10b101866ae0
artem-p <artempugachev1@gmail.com>
2017-06-09 13:08:17 +0300

    add recycler view var

--- app/src/main/java/ru/artempugachev/myweather/ForecastActivity.kt
+++ app/src/main/java/ru/artempugachev/myweather/ForecastActivity.kt
@@ -1,9 +1,11 @@
 package ru.artempugachev.myweather
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
 
 class ForecastActivity : DrawerActivity() {
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
