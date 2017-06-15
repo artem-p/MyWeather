00b960de109b587f6650ce0b1f49bab9e0524633
artem-p <artempugachev1@gmail.com>
2017-06-15 15:23:28 +0300

    recycler view with test strings

--- app/src/main/java/ru/artempugachev/myweather/ui/ForecastActivity.kt
+++ app/src/main/java/ru/artempugachev/myweather/ui/ForecastActivity.kt
@@ -1,18 +1,28 @@
 package ru.artempugachev.myweather.ui
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import ru.artempugachev.myweather.DrawerActivity
 import ru.artempugachev.myweather.R
 
 class ForecastActivity : DrawerActivity() {
     lateinit var recyclerView: RecyclerView
-    lateinit var adapter: RecyclerView.Adapter
+    lateinit var adapter: ForecastAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
 
         createDrawer()
+
+        recyclerView = findViewById(R.id.rv_forecast) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.setHasFixedSize(true)
+        adapter = ForecastAdapter()
+        recyclerView.adapter = adapter
+
+        val forecastStub = arrayOf("Forecast 1", "Forecast 2", "Forecast 3")
+        adapter.setData(forecastStub)
     }
 }
--- app/src/main/java/ru/artempugachev/myweather/ui/ForecastAdapter.kt
+++ app/src/main/java/ru/artempugachev/myweather/ui/ForecastAdapter.kt
@@ -1,23 +1,44 @@
 package ru.artempugachev.myweather.ui
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import ru.artempugachev.myweather.R
 
-class ForecastAdapter(val dataSet: Array<String>) : RecyclerView.Adapter<ForecastAdapter.ViewHolder>() {
-    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+class ForecastAdapter : RecyclerView.Adapter<ForecastAdapter.ForecastViewHolder>() {
+    lateinit var forecastData: Array<String>
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ForecastViewHolder {
+        val context = parent?.context
+        val inflater = LayoutInflater.from(context)
+        val shouldAttachToParentImmideately = false
+
+        val view = inflater.inflate(R.layout.forecast_list_item, parent, shouldAttachToParentImmideately)
+        return ForecastViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    override fun onBindViewHolder(holder: ForecastViewHolder?, position: Int) {
+        holder?.bindForecast(forecastData[position])
     }
 
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return forecastData.size
     }
 
+    fun setData(data: Array<String>) {
+        forecastData = data
+        notifyDataSetChanged()
+    }
+
+    class ForecastViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val forecastTextView: TextView = view.findViewById(R.id.forecastText) as TextView
+
+        fun bindForecast(forecast: String) {
+            forecastTextView.setText(forecast)
+        }
+    }
 }
